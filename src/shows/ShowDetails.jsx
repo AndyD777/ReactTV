733a22f3e58242e5c039be20a002b747561caf68
Andrew Depradine <andy.depradine@yahoo.com>
2025-05-05 18:05:56 -0400

    Update

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList.jsx";
+import EpisodeDetails from "../episodes/EpisodeDetails.jsx";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
 export default function ShowDetails() {
-  return <div className="show-details"></div>;
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return (
+      <section className="show-details">
+        <p>Please select a show to see details.</p>
+      </section>
+    );
+  }
+
+  return <div className="show-details">
+    <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+  </div>
+  ;
 }
